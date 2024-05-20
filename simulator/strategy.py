afc1567cf7604b4efef21ff86f25c8c1bcdbd6ab
stasi <stasi009@163.com>
2024-05-20 15:55:07 +0800

    save

--- simulator/strategy.py
+++ simulator/strategy.py
@@ -1,18 +1,16 @@
 from dataclasses import dataclass
-from typing import Tuple
-from pathlib import Path
+from datetime import datetime
 from simulator.data_feeds import DataFeeds
 from simulator.exchange import Exchange
 from simulator.arbitrage_trade import FundingArbTrade
 from simulator.config import Config
-import logging
-    
+
 
 @dataclass
 class ArbPair:
-    symbol: str
+    market: str
     long_ex: str
-    short_cex: str
+    short_ex: str
     fundrate_diff: float
 
 
@@ -37,10 +35,13 @@ class FundingArbStrategy:
         self._active_arb_trades: dict[str, FundingArbTrade] = {}  # market --> trade
         self._closed_trades: list[FundingArbTrade] = {}
 
-    def _best_arb_pair(self, market:str, ex_fundrates: dict[str, float]) -> ArbPair:
+    def _best_arb_pair(self, market: str, funding_rates: dict[str, dict[str, float]]) -> ArbPair:
         """
-        funding_rates: exchange -> price / funding rate
+        Args:
+            funding_rates: out-key=market, inner dict: exchange->price
         """
+        ex_fundrates = funding_rates[market]
+
         max_frate_diff = 0
         long_ex = None
         short_ex = None
@@ -58,55 +59,64 @@ class FundingArbStrategy:
                     short_ex = ex2_name if fundrate1 < fundrate2 else ex1_name
                     max_frate_diff = frate_diff
 
-        return long_ex, short_ex, max_frate_diff
+        return ArbPair(market=market, long_ex=long_ex, short_ex=short_ex, fundrate_diff=max_frate_diff)
 
-    def __open(self, market: str, long_ex: str, short_ex: str, fundrate_diff: float):
+    def __open(self, arbpair: ArbPair):
         """返回两个trade，第1个是要关闭的trade，第2个是要开仓或加仓的trade"""
         new_trade = FundingArbTrade(
-            market=market,
-            long_ex=self._exchanges[long_ex],
-            short_ex=self._exchanges[short_ex],
+            market=arbpair.market,
+            long_ex=self._exchanges[arbpair.long_ex],
+            short_ex=self._exchanges[arbpair.short_ex],
         )
 
-        if market not in self._active_arb_trades:
-            self._active_arb_trades[market] = new_trade
+        if arbpair.market not in self._active_arb_trades:
+            self._active_arb_trades[arbpair.market] = new_trade
             print(f"open new trade: {new_trade.name}")
             return None, new_trade
 
-        old_trade = self._active_arb_trades[market]
+        old_trade = self._active_arb_trades[arbpair.market]
 
         if (old_trade.name == new_trade.name) and (
-            fundrate_diff >= old_trade.open_fundrate_diff * (1 + self._config.fundrate_diff_change_pct)
+            arbpair.fundrate_diff
+            >= old_trade.open_fundrate_diff * (1 + self._config.fundrate_diff_change_pct)
         ):
             print(f"increase position on {new_trade.name}")
             return None, old_trade  # 加仓
 
         if (old_trade.name != new_trade.name) and (
-            fundrate_diff >= old_trade.latest_fundrate_diff * (1 + self._config.fundrate_diff_change_pct)
+            arbpair.fundrate_diff
+            >= old_trade.latest_fundrate_diff * (1 + self._config.fundrate_diff_change_pct)
         ):
             print(f"change trade from {old_trade.name} to {new_trade.name}")
             # 关闭active_trade，开仓new_trade
             return old_trade, new_trade
 
-    def open(self, prices: dict[str, dict[str, float]], funding_rates: dict[str, dict[str, float]]):
+        return None, None
+
+    def open(
+        self, tm: datetime, prices: dict[str, dict[str, float]], funding_rates: dict[str, dict[str, float]]
+    ):
         """
         Args:
             prices:        out-key=market, inner dict: exchange->price
             funding_rates: out-key=market, inner dict: exchange->funding rate
         """
         for market in self._config.markets:
-            long_ex, short_ex, fundrate_diff = self._best_arb_pair(ex_fundrates=funding_rates[market])
-            if fundrate_diff < self._config.fundrate_diff_open:
+            arbpair = self._best_arb_pair(ex_fundrates=funding_rates[market])
+            if arbpair.fundrate_diff < self._config.fundrate_diff_open:
                 continue
-            
-            trade2close, trade2open = self.__open(market=market, 
-                                                  long_ex=long_ex, 
-                                                  short_ex=short_ex, fundrate_diff: float)
 
-            trade2open.open(
-                usd_amount=self._config.ordersize_usd,
-                prices=prices[market],
-            )
+            trade2close, trade2open = self.__open(arbpair)
+            if trade2close is not None:
+                trade2close.close(tm, prices[market])
+
+            if trade2open is not None:
+                trade2open.open(
+                    tm=tm,
+                    usd_amount=self._config.ordersize_usd,
+                    prices=prices[market],
+                    fundrate_diff=arbpair.fundrate_diff,
+                )
 
     def close(self, prices: dict[str, dict[str, float]], funding_rates: dict[str, dict[str, float]]):
 
