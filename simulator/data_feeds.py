04525ffa813b4ffdfe0b34075441b8bc572571c6
stasi <stasi009@163.com>
2024-05-21 16:11:17 +0800

    save

--- simulator/data_feeds.py
+++ simulator/data_feeds.py
@@ -2,53 +2,83 @@ import pandas as pd
 from pathlib import Path
 from dataclasses import dataclass
 from datetime import datetime
+from collections import defaultdict
 
 
-class FeedOnce:
+class FeedOnce:  # 某个时刻下的数据
     def __init__(self) -> None:
         self.timestamp: datetime = None
-        self.prices: dict[str, dict[str, float]] = {}
-        self.funding_rates: dict[str, dict[str, float]] = {}
+        # 外层key是market，内层dict是exchange -> price / funding rate
+        self.open_prices: dict[str, dict[str, float]] = defaultdict(dict)
+        self.close_prices: dict[str, dict[str, float]] = defaultdict(dict)
+        self.mark_prices: dict[str, dict[str, float]] = defaultdict(dict)
+        self.funding_rates: dict[str, dict[str, float]] = defaultdict(dict)
+        self.__col2container = {
+            "open_price": self.open_prices,
+            "close_price": self.close_prices,
+            "mark_price": self.mark_prices,
+            "fund_rate": self.funding_rates,
+        }
+
+    def add(self, column: str, market: str, exchange: str, value: float):
+        container = self.__col2container[column]
+        # 外层key是market，内层dict是exchange -> value
+        container[market][exchange] = value
 
 
 class DataFeeds:
-    def __init__(self, data_dir: Path, cex_list: list[str], symbol_list: list[str]) -> None:
+    def __init__(self, data_dir: Path, exchanges: list[str], markets: list[str]) -> None:
         self._datas = {}
-        self._symbol_list = symbol_list
+        self._exchanges = exchanges
+        self._markets = markets
         self._index = 0
-        
+
         self._total_rows = None
-        for cex in cex_list:
-            fname = data_dir / f"{cex}.csv"
-            df = pd.read_csv(fname, index_col="timestamp", parse_dates=True)
+        for ex in exchanges:
+            for market in markets:
 
-            if self._total_rows is None:
-                self._total_rows = df.shape[0]
-            else:
-                assert self._total_rows == df.shape[0], "all CEX should have same length of data"
+                fname = data_dir / f"{ex}/{market}.csv"
+                df = pd.read_csv(fname, index_col="timestamp", parse_dates=True)
 
-            self._datas[cex] = df
+                if self._total_rows is None:
+                    self._total_rows = df.shape[0]
+                else:
+                    assert self._total_rows == df.shape[0], "all exchanges should have same length of data"
+
+                self._datas[f"{ex}/{market}"] = df
 
     def __iter__(self):
         return self
 
-    def __next__(self):
-        if self._index >= self._total_rows:
-            raise StopIteration
-
+    def __read_current(self):
         feed = FeedOnce()
 
-        for cex, df in self._datas.items():
-            row = df.iloc[self._index, :]
+        for market in self._markets:
+            for ex in self._exchanges:
+                df = self._datas[f"{ex}/{market}"]
+                row = df.iloc[self._index, :]
 
-            if feed.timestamp is None:
-                feed.timestamp = row.name.to_pydatetime()
-            else:
-                assert feed.timestamp == row.name.to_pydatetime()
+                # TODO:只要存在NaN，就放弃这个timestamp的所有market+exchange的数据
+                if row.isna().any():
+                    return None
 
-            feed.prices[cex] = {symbol: row[symbol + "_price"] for symbol in self._symbol_list}
-            feed.funding_rates[cex] = {symbol: row[symbol + "_fundrate"] for symbol in self._symbol_list}
+                if feed.timestamp is None:
+                    feed.timestamp = row.name.to_pydatetime()
+                else:
+                    assert feed.timestamp == row.name.to_pydatetime()
 
-        self._index += 1
+                for col in ["open_price", "close_price", "mark_price", "fund_rate"]:
+                    feed.add(column=col, market=market, exchange=ex, value=row[col])
 
         return feed
+
+    def __next__(self):
+        while self._index < self._total_rows:
+            feed = self.__read_current()
+
+            self._index += 1
+
+            if feed is not None:
+                return feed
+
+        raise StopIteration
